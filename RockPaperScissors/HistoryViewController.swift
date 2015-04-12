03d2125d7f04e1de534ef443f2035938b6a5c524
Chris Archibald <carchi8py@gmail.com>
2015-04-12 13:10:47 -0700

    Added History table

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -9,9 +9,34 @@
 import Foundation
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDataSource {
     
     var history: [RPSMatch]!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.history.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("HistoryCell") as! UITableViewCell
+        let game = self.history[indexPath.row]
+        
+        //If the player beats the computer
+        if game.p1.defeats(game.p2) {
+            cell.textLabel?.text = "Win!"
+        } else {
+            cell.textLabel?.text = "Lose."
+        }
+        
+        if let detailTextLable = cell.detailTextLabel {
+            detailTextLable.text = game.p1.description + " vs " + game.p2.description
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
     
     
     @IBAction func backToHome(sender: AnyObject) {
